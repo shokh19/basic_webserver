69d7520b5b20ef273b9337e0593d6f9a14d30bb7
shokh19 <60832928+shokh19@users.noreply.github.com>
2022-05-02 18:29:03 +0900

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -26,14 +26,17 @@ def start_server():
 
 def load_page_from_get_request(request_data):
     HDRS = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\n\r\n'
+    HDRS_404 = 'HTTP/1.1 404 OK\r\nContent-type: text/html; charset=utf-8\r\n\r\n'
     path = request_data.split(' ')[1]
     response = ''
-    if path == '/':
-        path = '/index.html'
-    with open('htdocs'+path, 'rb') as file:
-        response = file.read()
-    return HDRS.encode('utf-8') + response
-
+    try:
+        if path == '/':
+            path = '/index.html'
+        with open('htdocs'+path, 'rb') as file:
+            response = file.read()
+        return HDRS.encode('utf-8') + response
+    except FileNotFoundError:
+        return (HDRS_404 + 'Sorry, buddy! No page found...').encode('utf-8')
 
 if __name__=='__main__':
     start_server()
